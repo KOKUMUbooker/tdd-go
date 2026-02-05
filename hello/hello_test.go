e8df0c5f4181df663e599185535adced7e74eb1c
KOKUMUbooker <bookerochieng20@gmail.com>
2026-02-05 13:04:26 +0300

    Add test for empty string

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -11,8 +11,23 @@ import "testing"
 // $ go doc fmt
 
 func TestHello(t *testing.T) {
-	got := Hello("John")
-	want := "Hello, John"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("empty string defaults to 'world'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	// To tell the test suite that this is a helper method so that the error line reported as point of failure
+	// is where func is called & not in the helper
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
